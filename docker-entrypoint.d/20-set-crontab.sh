8c036ee7ddfc6975f35b88fedd40dde3a0e23752
Niels Højen <niels.hojen@bestseller.com>
2022-01-05 16:54:16 +0100

    update if we already have data

--- docker-entrypoint.d/20-set-crontab.sh
+++ docker-entrypoint.d/20-set-crontab.sh
@@ -6,6 +6,6 @@ if [ ! -z "${CRONTAB}" ] && [ ! -z "${SOURCE}" ] && [ ! -z "${TARGET}" ]; then
   echo "git -C ${TARGET} lfs prune" >> $HOME/scheduled_fetch.sh
   echo "Pull completed - $(date) >> $HOME/running.log" >> $HOME/scheduled_fetch.sh
   echo "exit 0" >> $HOME/scheduled_fetch.sh
-  (crontab -l 2>/dev/null; echo "${CRONTAB} $HOME/scheduled_fetch.sh") | crontab -
+  (crontab -l 2>/dev/null; echo "${CRONTAB} $HOME/scheduled_fetch.sh >/dev/null 2>&1") | crontab -
   echo "Crontab job added" >> $HOME/running.log
 fi
--- docker-entrypoint.d/30-clone.sh
+++ docker-entrypoint.d/30-clone.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ ! -z "${SOURCE}" ] && [ ! -z "${TARGET}" ]; then
-    git clone ${CLONE_OPTS} ${SOURCE} ${TARGET} 
+    if [ "$(ls -A ${TARGET})" ]; then
+        git -C ${TARGET} pull
+    else
+        git clone ${CLONE_OPTS} ${SOURCE} ${TARGET}
+    fi
 else
     echo "No Source and/or Target supplied"
 fi
