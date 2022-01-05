39ee1b2a5f0f5bb7a543592856bbdbbd943f3c19
Niels Højen <niels.hojen@bestseller.com>
2022-01-05 20:50:19 +0100

    set bits

--- docker-entrypoint.d/20-set-crontab.sh
+++ docker-entrypoint.d/20-set-crontab.sh
@@ -2,11 +2,12 @@
 
 if [ ! -z "${CRONTAB}" ] && [ ! -z "${SOURCE}" ] && [ ! -z "${TARGET}" ]; then
   crontab -e ${USER}
-  echo "#!/bin/sh" > $HOME/scheduled_fetch.sh
-  echo "git -C ${TARGET} pull" >> $HOME/scheduled_fetch.sh
-  echo "git -C ${TARGET} lfs prune" >> $HOME/scheduled_fetch.sh
-  echo "Pull completed - $(date) >> $HOME/running.log" >> $HOME/scheduled_fetch.sh
-  echo "exit 0" >> $HOME/scheduled_fetch.sh
-  (crontab -l 2>/dev/null; echo "${CRONTAB} $HOME/scheduled_fetch.sh >/dev/null 2>&1") | crontab -
+  echo "#!/bin/sh" > $HOME/cronjob.sh
+  echo "git -C ${TARGET} pull" >> $HOME/cronjob.sh
+  echo "git -C ${TARGET} lfs prune" >> $HOME/cronjob.sh
+  echo "echo \"Pull completed - \$(date)\" >> $HOME/running.log" >> $HOME/cronjob.sh
+  echo "exit 0" >> $HOME/cronjob.sh
+  chmod +x $HOME/cronjob.sh
+  (crontab -l 2>/dev/null; echo "${CRONTAB} $HOME/cronjob.sh >/dev/null 2>&1") | crontab -
   echo "Crontab job added" >> $HOME/running.log
 fi
