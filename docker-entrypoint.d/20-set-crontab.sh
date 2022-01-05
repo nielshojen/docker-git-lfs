c58c7ecd433ddf498e2588910fecf3226d055708
Niels Højen <niels.hojen@bestseller.com>
2022-01-05 13:55:34 +0100

    fix tests

--- docker-entrypoint.d/20-set-crontab.sh
+++ docker-entrypoint.d/20-set-crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [${CRONTAB} ] && [ ${SOURCE} ] && [ ${TARGET} ]; then
+if [ ! -z "${CRONTAB}" ] && [ ! -z "${SOURCE}" ] && [ ! -z "${TARGET}" ]; then
   echo "#!/bin/sh" > $HOME/scheduled_fetch.sh
   echo "git -C ${TARGET} pull" >> $HOME/scheduled_fetch.sh
   echo "git -C ${TARGET} lfs prune" >> $HOME/scheduled_fetch.sh
